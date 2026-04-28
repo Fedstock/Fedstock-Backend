1fe31652e1a4a1002e7e6635e97d9a8447375ae4
Ohjackson <wogus3673@naver.com>
2026-04-28 19:37:44 +0900

    feat: initialize spring boot settings

---
+++ src/main/java/com/fedstock/backend/FedstockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fedstock.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedstockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedstockBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedstock/backend/FedstockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedstock.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedstockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
